15bdfe93e85abae18d49c91161a7f56fbab58a7d
Liara Bessa <liara_lia@hotmail.com>
2024-05-02 14:18:07 -0300

    aprendendo os operadores em java

---
+++ curso/src/TiposOperadores.java
@@ -0,0 +1,20 @@
+public class TiposOperadores {
+   // = declara
+   // == igual
+   // != diferente
+   // / divisao
+   // * multiplicacao
+   // % modulo - resto da divisao
+   // boolean - false ou true
+   // new Date ()
+
+//    int divisao = 15 / 3;
+//    int modulo = 18 % 3;
+//    double resultado = (10 *5) + (20 / 4);
+
+   public static void main(String[] args) {
+       String nomeCompleto = "LINGUAGEM " + "JAVA";
+
+       System.out.println(nomeCompleto);
+   }
+}
