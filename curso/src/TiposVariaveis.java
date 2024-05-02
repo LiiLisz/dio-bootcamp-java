24c30d330136cc185089398ef094cf6becb28ca2
Liara Bessa <liara_lia@hotmail.com>
2024-05-02 13:58:25 -0300

    algumas variaveis

--- curso/src/TiposVariaveis.java
+++ curso/src/TiposVariaveis.java
@@ -1,3 +1,25 @@
+
+import java.awt.SystemTray;
+
 public class TiposVariaveis {
+    // Tipos de variaveis:
+    // int numero normal
+    // short numero curto
+    // long numero alto
+    // float
+    // double
+
+    // <tipo> <nomeVariavel> = <valorAtribuido>;
+
+    // byte idade = 102;
+    // short ano = 2024;
+    // int cep = 74250270;
+    // long cpf = 03738031170L;
+    // float pi = 3.14F;
+    // double salarioMinimo = 1429.90;
     
+    // constantes (não modifica depois de declarado em 
+    // CAIXA ALTA)
+    
+    // final double VALOR DE PI = 3.14;
 }
