016a39218ddfa838bc312bae942316207179ad81
Liara Bessa <liara_lia@hotmail.com>
2024-05-06 15:12:31 -0300

    projetinho da smartv

---
+++ curso/src/sistema-smartv/SmartTv.java
@@ -0,0 +1,16 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 50;
+
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+}
